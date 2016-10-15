97019fda7afc0941cbae4fb0b70e579c09058776
danhlutz <danhlutz@gmail.com>
2016-10-14 21:17:45 -0400

    Write pseudocode for first part of the project, scraping a single page

---
+++ bot.py
@@ -0,0 +1,39 @@
+#trot_bot
+
+# scrape a page
+
+# get a request from the Trotsky Internet Archive
+
+# divide up the request into words
+
+# zip(doc, doc[1:], doc[2:]
+# add doc and doc[1:] to a dict, and append doc[2:]
+
+# if doc == punctuation, add doc[1:] and doc[2:] to start words
+
+
+
+# generate a random text string
+
+# pick a start word at random
+
+# keep going until you get a punctuation mark
+
+
+
+
+
+
+
+
+
+#LATER STUFF
+
+# scrape the whole TIA
+# first scrape for links. Build up a list of link
+
+# pull a sample of k links, scrape all of them
+
+# tweet
+
+
