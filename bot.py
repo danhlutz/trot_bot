e6c009a10360109d53ba729983aca86923c4ee0c
danhlutz <danhlutz@gmail.com>
2016-10-16 20:19:04 -0400

    Add generate_words function to Bot class

--- bot.py
+++ bot.py
@@ -43,19 +43,22 @@ class Bot:
                 (self.start_words).append(current)
             self.trigrams[(prev, current)].append(next)
 
+    def generate_words(self):
+        current = random.choice(self.start_words)
+        prev = '.'
+        result = [current]
+        while True:
+            next_word = random.choice(self.trigrams[(prev, current)])
+            prev, current = current, next_word
+            result.append(current)
+            if current in ['.', '?', '!']:
+                final = " ".join(result)
+                return final[:-2] + current
+            
 
-# zip(doc, doc[1:], doc[2:]
-# add doc and doc[1:] to a dict, and append doc[2:]
-
-# if doc == punctuation, add doc[1:] and doc[2:] to start words
-
-
-
-# generate a random text string
-
-# pick a start word at random
-
-# keep going until you get a punctuation mark
+    # generate a random text string
+    # pick a start word at random
+    # keep going until you get a punctuation mark
 
 
 
